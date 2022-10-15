c928743e30f3f90e555619c2a63e9de0538a68cd
sorinboia <sorinboia@gmail.com>
2022-10-15 10:37:11 +0300

    1

--- udf/aws_microk8s.tf
+++ udf/aws_microk8s.tf
@@ -15,7 +15,7 @@ resource "aws_instance" "microk8s" {
   associate_public_ip_address = true
   key_name                    = aws_key_pair.aws_key.key_name
   
-  availability_zone           = var.aws_az
+  availability_zone           = var.aws_az1
   subnet_id                   = aws_subnet.public-subnet.id
   vpc_security_group_ids      = [aws_security_group.sgweb.id]
 
