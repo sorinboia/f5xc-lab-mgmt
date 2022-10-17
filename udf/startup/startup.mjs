09a72591052f0ce60c7a708f4aa67e04a3654b54
sorinboia <sorinboia@gmail.com>
2022-10-17 16:43:56 +0300

    1

--- udf/startup/startup.mjs
+++ udf/startup/startup.mjs
@@ -141,7 +141,7 @@ const installAwsMicrok8s = async () => {
           'sudo usermod -a -G microk8s ubuntu"');
     exec(`ssh -o "StrictHostKeyChecking no" -i ~/.ssh/aws.key ubuntu@${tfOutput.microk8s_ip.value} "microk8s config" > ~/.kube/config`);
     exec(`sed -i 's/certificate-authority-data.*//g' ~/.kube/config`);
-    exec(`sed -i 's/server.*16443/server: https:\/\/${tfOutput.microk8s_ip.value}:16443\\n    insecure-skip-tls-verify: true/g' ~/.kube/config`);
+    exec(`sed -i 's/server.*16443/server: https:\\/\\/${tfOutput.microk8s_ip.value}:16443\\n    insecure-skip-tls-verify: true/g' ~/.kube/config`);
     exec('kubectl apply -f aws_microk8s/*')
 
     state = 1;
