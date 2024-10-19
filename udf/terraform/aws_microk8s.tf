73c7b6e208790636dd948119d6302ea899cf86fc
Sorin Boiangiu <sorinboia@gmail.com>
2024-10-19 10:35:51 +0300

    1

--- udf/terraform/aws_microk8s.tf
+++ udf/terraform/aws_microk8s.tf
@@ -1,9 +1,15 @@
 data "aws_ami" "ubuntu" {
   most_recent = true
+  owners      = ["099720109477"] # Canonical
 
   filter {
     name   = "name"
-    values = ["*ubuntu*22.04*amd64*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-20240927"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
   }
 }
 
