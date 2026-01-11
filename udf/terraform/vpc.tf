b8c6695a9db36b177c209bda590a2af1a7e45110
Sorin Boiangiu <sorinboia@gmail.com>
2026-01-11 09:35:56 +0200

    remove dg in public route table

--- udf/terraform/vpc.tf
+++ udf/terraform/vpc.tf
@@ -52,10 +52,10 @@ resource "aws_internet_gateway" "gw" {
 resource "aws_route_table" "web-public-rt" {
   vpc_id = aws_vpc.main.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw.id
-  }
+  # route {
+  #   cidr_block = "0.0.0.0/0"
+  #   gateway_id = aws_internet_gateway.gw.id
+  # }
 
   tags = {
     Name = "Public Subnet RT"
